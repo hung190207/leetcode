43ef1d267872af0b58dc8cc491dd864157997bb6
hung190207 <hung192@gmail.com>
2021-08-03 23:18:13 +0700

    Done problem 326

--- p326/power_of_three.c
+++ p326/power_of_three.c
@@ -2,21 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool power_of_three(int n) {
-  if (n == 0) return 0;
-  while (n != 1) {
-    if (n % 3 != 0) {
-      return 0;
+bool isPowerOfThree(int n) {
+  // 3486784401u is 3^20 and the largest power of 3 that can be represented in
+  // 32bit unsigned
+
+  // unsigned int
+  // n == 3^k
+  // x = 3^(k+i) > n; x % n == 0
+  return n != 0 && 3486784401u % n == 0;
+
+  /*
+    if (n == 0) return 0;
+    while (n != 1) {
+      if (n % 3 != 0) {
+        return 0;
+      }
+      n = n / 3;
     }
-    n = n / 3;
-  }
-  return 1;
+    return 1;
+    */
 }
 
-bool isPowerOfThree(int n) { return power_of_three((int)n); }
-
 void test(int n, bool wanted) {
-  bool actual = power_of_three(n);
+  bool actual = isPowerOfThree(n);
   if (actual != wanted) {
     printf("ERROR: num=%d, wanted=%d but actual=%d \n", n, wanted, actual);
   }
