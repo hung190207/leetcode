eec176628db5b7ef35d1b9886f068498ae39bcdf
hung190207 <hungcs192@gmail.com>
2021-08-10 04:52:54 +0700

    Improve fibonacci algorithm

--- p509/fibonacci_number.c
+++ p509/fibonacci_number.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 
-int arr[31] = {0,1};
-int fib(int n)
-{
-    for(int i=2;i<=n;i++)
-    {
-        arr[i] = arr[i-1] + arr[i-2] ;
+int fib(int n) {
+    if (n < 0) {
+        return 0;
     }
-    
-    return arr[n];
+
+    if (n <= 1) {
+        return n;
+    }
+
+    int a = 0;
+    int b = 1;
+    for (int i = 2; i <= n; i++) {
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
 }
 
-void test(int n , int wanted){
+void test(int n, int wanted) {
     int actual = fib(n);
-    if(actual != wanted){
-        printf("ERROR:n = %d , wanted = %d but actual = %d\n",n,wanted,actual);
+    if (actual != wanted) {
+        printf("ERROR:n = %d , wanted = %d but actual = %d\n", n, wanted,
+               actual);
     }
 }
 
-int main(){
-    test(2,1);
-    test(3,2);
-    test(4,3);
-    test(30,832040);
+int main() {
+    int fib_numbers[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+    int n = sizeof(fib_numbers) / sizeof(int);
+    for (int i = 0; i < n; i++) {
+        int f = fib_numbers[i];
+        test(i, f);
+    }
+
+    test(30, 832040);
 }
