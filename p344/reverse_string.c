ca378bb274e2ac923f2bbc5c3b41e36b96c46e4d
hung190207 <hungcs192@gmail.com>
2021-08-10 04:47:14 +0700

    Add test cases and Makefile for p344

--- p344/reverse_string.c
+++ p344/reverse_string.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// https://leetcode.com/problems/reverse-string/
+
 //     i
 // a b c
 // j
-//Runtime: 48 ms, faster than 74.91% of C online submissions for Reverse String.
+// Runtime: 48 ms, faster than 74.91% of C online submissions for Reverse
+// String.
 
-char* reverseString(char* s, int size) {
+void reverseString(char* s, int size) {
     int i = 0;
     int j = size - 1;
 
@@ -15,5 +21,24 @@ char* reverseString(char* s, int size) {
         i++;
         j--;
     }
-    return s;
+}
+
+void test(char* s, int size, char* wanted) {
+    char* copy = (char*)malloc(size + 1);
+    strcpy(copy, s);
+    reverseString(copy, size);
+
+    for (int i = 0; i < size; i++) {
+        if (copy[i] != wanted[i]) {
+            printf("ERROR: input=%s, actual=%s but wanted=%s\n", s, copy,
+                   wanted);
+            return;
+        }
+    }
+}
+
+int main() {
+    test("abc", 3, "cba");
+    test("hello", 5, "olleh");
+    test("nguyen ngoc hung", 16, "gnuh cogn neyugn");
 }
