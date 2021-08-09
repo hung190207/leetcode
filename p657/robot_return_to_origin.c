33826a83a349b9c5f372302f8fd677ec14383cef
hung190207 <hungcs192@gmail.com>
2021-08-10 05:07:11 +0700

    Add a document

--- p657/robot_return_to_origin.c
+++ p657/robot_return_to_origin.c
@@ -1,57 +1,61 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 
-// TEST 1 : Runtime: 7 ms, faster than 39.56% of C online submissions for Robot Return to Origin.
+// TEST 1 : Runtime: 7 ms, faster than 39.56% of C online submissions for Robot
+// Return to Origin.
 
-// TEST 2 : Runtime: 2 ms, faster than 97.80% of C online submissions for Robot Return to Origin.
+// TEST 2 : Runtime: 2 ms, faster than 97.80% of C online submissions for Robot
+// Return to Origin.
 
-// TEST 3 : Runtime: 0 ms, faster than 100.00% of C online submissions for Robot Return to Origin.
+// TEST 3 : Runtime: 0 ms, faster than 100.00% of C online submissions for Robot
+// Return to Origin.
 
-bool judgeCircle(char* m) {
-    if(m == NULL){
+// TODO (hung): solve it again without look up on the internet
+
+bool judgeCircle(char *m) {
+    if (m == NULL) {
         return true;
     }
-    
+
     int len = (int)strlen(m);
-    if(len % 2)
-    {
+    if (len % 2) {
         return false;
     }
-    
+
     int count[2] = {0};
-    for(int i = 0; i<len; i++)
-    {
-        if(m[i] == 'L'){
+    for (int i = 0; i < len; i++) {
+        if (m[i] == 'L') {
             count[0]++;
-        }else if(m[i] == 'R'){
+        } else if (m[i] == 'R') {
             count[0]--;
-        }else if(m[i] == 'U'){
+        } else if (m[i] == 'U') {
             count[1]++;
-        }else if(m[i] == 'D'){
+        } else if (m[i] == 'D') {
             count[1]--;
         }
     }
-    if(count[0] == 0 && count[1] == 0){
+    if (count[0] == 0 && count[1] == 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-char* bool2str(bool b) { return b ? "true" : "false"; }
+char *bool2str(bool b) { return b ? "true" : "false"; }
 
-void test(char* m,char wanted){
+void test(char *m, char wanted) {
     char actual = judgeCircle(m);
-    if(actual != wanted){
-        printf("ERROR:m = %s,wanted = %s but actual = %s\n",m,bool2str(wanted),bool2str(actual));
+    if (actual != wanted) {
+        printf("ERROR:m = %s,wanted = %s but actual = %s\n", m,
+               bool2str(wanted), bool2str(actual));
     }
 }
 
-int main(){
-    test("UD",true);
-    test("LL",false);
-    test("RRDD",false);
-    test("LDRRLRUULR",false);
-    test("UDDLUURD",true);
+int main() {
+    test("UD", true);
+    test("LL", false);
+    test("RRDD", false);
+    test("LDRRLRUULR", false);
+    test("UDDLUURD", true);
 }
