0b97903a6ccc23cdfd61403a3f913af605b8bfa3
hung190207 <hungcs192@gmail.com>
2021-08-30 21:43:00 +0700

    library for some problems.

---
+++ lib/list.h
@@ -0,0 +1,78 @@
+#ifndef __LIST__
+#define __LIST__
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode* next;
+};
+
+struct ListNode* makeList(int n, ...) {
+    struct ListNode pre = {};
+    struct ListNode* parent = &pre;
+
+    va_list arr;
+    va_start(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        struct ListNode* cur =
+            (struct ListNode*)calloc(1, sizeof(struct ListNode));
+        cur->val = va_arg(arr, int);
+        parent->next = cur;
+        parent = cur;
+    }
+
+    va_end(arr);
+    return pre.next;
+}
+
+void print_list(struct ListNode* head) {
+    if (head == NULL) {
+        printf("\n");
+        return;
+    }
+
+    printf("%d", head->val);
+    head = head->next;
+
+    while (head != NULL) {
+        printf(" -> %d", head->val);
+        head = head->next;
+    }
+
+    printf("\n");
+}
+
+bool assert_list_equal(struct ListNode* wanted, struct ListNode* actual) {
+     struct ListNode* a = actual;
+    struct ListNode* w = wanted;
+    while (a != NULL && w != NULL) {
+        if (a->val != w->val) {
+            printf("[ERROR] wanted val=%d, actual=%d\n", w->val, a->val);
+            printf("[ERROR] actual: ");
+            print_list(actual);
+            printf("[ERROR] wanted: ");
+            print_list(wanted);
+            return false;
+        }
+        w = w->next;
+        a = a->next;
+    }
+
+    if (a != NULL || w != NULL) {
+        printf("[ERROR] 2 lists have different size\n");
+        printf("[ERROR] actual: ");
+        print_list(actual);
+        printf("[ERROR] wanted: ");
+        print_list(wanted);
+        return false;
+    }
+
+    return true;
+}
+
+#endif
